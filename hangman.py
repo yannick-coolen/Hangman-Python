e96c3a28c3ba28c87deafbde374b405d928e2379
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2022-02-23 16:00:53 +0100

    Upgraded the hangman game, user can only a one alpha character

--- hangman.py
+++ hangman.py
@@ -5,14 +5,14 @@ with open('words.txt', 'r') as f:
     allText = f.read()
     words = list(map(str, allText.split()))
     
-
 # set the random word ready for the hangman game
-
 secretWord = random.choice(words)
 
 amountOfTurns = 9
 guesses = []
 done = False
+
+
 
 while not done:
     for letter in secretWord:
@@ -20,10 +20,19 @@ while not done:
             print(letter, end=" ")
         else:
             print("_", end=" ")
-    print(guesses)  
+    print(guesses, end=", ")  
     
     guess = input(f'Player has {amountOfTurns} turn(s) left, Next Guess: ')
-    guesses.append(guess.lower())
+    
+    if guess.isalpha():
+        guesses.append(guess.lower())
+    else: 
+        print(f'{guess} is invalid! Please, enter one valid character')
+        amountOfTurns += 1    
+        
+    if len(guess) > 1:
+        print('Please, enter one valid character')
+        amountOfTurns += 1 
     
     if guess.lower() not in secretWord.lower():
         amountOfTurns -= 1
@@ -36,6 +45,6 @@ while not done:
             done = False
             
 if done:
-    print(f'You found the word! It was {secretWord}')
+    print(f'You Won! The word was {secretWord}')
 else: 
     print(f'Game Over, you lose! The word was {secretWord}')
